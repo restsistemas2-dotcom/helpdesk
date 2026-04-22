38df33b2e6d05a5faca3225dc224fd60fe47dcdc
ANALISTA IT <ANALISTA IT@100MCW.LOCAL>
2026-04-22 16:55:56 -0600

    debug on

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xg*yu^@3n^)rx=_$n!)t$c8=*(5lj=$c=u2!u&=4x-#z@9=xif'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
