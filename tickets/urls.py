37ca1cf798320d351b5bec36ae6d7193c097b357
ANALISTA IT <ANALISTA IT@100MCW.LOCAL>
2026-04-27 16:05:57 -0600

    Fix admin register models

--- tickets/urls.py
+++ tickets/urls.py
@@ -6,13 +6,14 @@ from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
 from tickets.admin import admin_site
+from django.contrib import admin
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='tickets/login.html'), name='login'),
     path('tickets/', views.lista_tickets, name='lista_tickets'),
     path('crear/', views.crear_ticket, name='crear_ticket'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('admin/', admin_site.urls),
+    path('admin/', admin.site.urls),
     path('ajax/subcategorias/', views.cargar_subcategorias, name='cargar_subcategorias'),
     path('crear-admin/', crear_admin),
     path('logout/', LogoutView.as_view(), name='logout'),
