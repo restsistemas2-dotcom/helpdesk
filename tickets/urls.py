33dfd743b353a147cb7270a8e6ce285cb73d03fd
ANALISTA IT <ANALISTA IT@100MCW.LOCAL>
2026-04-27 11:44:10 -0600

    fix login template

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,7 +4,7 @@ from .views import crear_admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='tickets/login.html'), name='login'),
     path('tickets/', views.lista_tickets, name='lista_tickets'),
     path('crear/', views.crear_ticket, name='crear_ticket'),
     path('crear-admin/', crear_admin),
