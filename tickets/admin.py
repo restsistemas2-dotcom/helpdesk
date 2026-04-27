a0c7e1f9a7f83da26e5aa009d4e92d13d8630fc2
ANALISTA IT <ANALISTA IT@100MCW.LOCAL>
2026-04-27 16:46:29 -0600

    add file upload to tickets

--- tickets/admin.py
+++ tickets/admin.py
@@ -6,6 +6,17 @@ from django.core.exceptions import ValidationError
 from django.urls import path
 from django.shortcuts import redirect
 
+# Guardar el método original
+original_get_urls = admin.site.get_urls
+
+def custom_get_urls():
+    urls = original_get_urls()
+    custom_urls = [
+        path('dashboard/', lambda request: redirect('/dashboard/'))
+    ]
+    return custom_urls + urls
+
+admin.site.get_urls = custom_get_urls
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
@@ -55,7 +66,3 @@ admin.site.register(Subcategoria)
 
 def dashboard_redirect(request):
     return redirect('/dashboard/')
-
-admin.site.get_urls = lambda: [
-    path('dashboard/', dashboard_redirect)
-] + admin.site.get_urls()
