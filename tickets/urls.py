dd8719cf2ce378164b2a05dee11b0a52bb99432e
ANALISTA IT <ANALISTA IT@100MCW.LOCAL>
2026-04-27 11:57:25 -0600

    mejora login template

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 from .views import crear_admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='tickets/login.html'), name='login'),
     path('tickets/', views.lista_tickets, name='lista_tickets'),
     path('crear/', views.crear_ticket, name='crear_ticket'),
     path('crear-admin/', crear_admin),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
