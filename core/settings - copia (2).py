10b14e72af9608478bbdc31a2c87f1c7539d26e9
ANALISTA IT <ANALISTA IT@100MCW.LOCAL>
2026-05-05 17:08:49 -0600

    correo corporativo

--- core/settings - copia (2).py
+++ core/settings - copia (2).py
@@ -149,12 +149,12 @@ LOGIN_REDIRECT_URL = '/tickets/'
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'emontenegro@100montaditosca.com'
-EMAIL_HOST_PASSWORD = '50LyXW2uJ2'
+EMAIL_HOST_USER = 'sistemas100mcw@gmail.com'
+EMAIL_HOST_PASSWORD = 'jgcz jvat gipb khmf'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
